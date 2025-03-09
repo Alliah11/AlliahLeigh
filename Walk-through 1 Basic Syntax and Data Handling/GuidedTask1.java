8ae62e269ee9732c2d837c0b254b2013a39afea0
Alliah11 <alliahleighgonzales155@gmail.com>
2025-03-08 23:58:13 -0800

    Add files via upload

---
+++ Walk-through 1 Basic Syntax and Data Handling/GuidedTask1.java
@@ -0,0 +1,6 @@
+public class GuidedTask1 {
+    public static void main(String[] args) {
+        String name = "Alliah Leigh";
+        System.out.println("Hello, " + name + " Ilov!");
+    }
+}
---
+++ Walk-through 1 Basic Syntax and Data Handling/GuidedTask2.java
@@ -0,0 +1,9 @@
+public class GuidedTask2 {
+    public static void main(String[] args) {
+        int length = 7;
+        int width = 8;
+        int area = length * width;
+
+        System.out.println("The area of the rectangle is: " + area);
+    }
+}
